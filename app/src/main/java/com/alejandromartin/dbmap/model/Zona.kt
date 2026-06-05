4b20e667f0acded81cda481643aa3eed3358f0ab
alejandromartin <alejandromartin824@gmail.com>
2026-06-06 00:04:32 +0200

    Elimina datos de latitud y longitud exactas

--- app/src/main/java/com/alejandromartin/dbmap/model/Zona.kt
+++ app/src/main/java/com/alejandromartin/dbmap/model/Zona.kt
@@ -1,9 +1,6 @@
 package com.alejandromartin.dbmap.model
 
 data class Zona(
-    val zonaId: String = "",
     val geohashReducido: String = "",
-    val nombreZona: String? = null,
-    val centroLatitud: Double = 0.0,
-    val centroLongitud: Double = 0.0
+    val ultimaActualizacion: Long = System.currentTimeMillis()
 )
