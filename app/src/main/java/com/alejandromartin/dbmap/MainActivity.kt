0a4fbf5fffbb4704106bf646bebd9b207ecc8cb2
alejandromartin <alejandromartin824@gmail.com>
2026-06-05 13:28:49 +0200

    Añade menu de navegacion para ventanas basicas

--- app/src/main/java/com/alejandromartin/dbmap/MainActivity.kt
+++ app/src/main/java/com/alejandromartin/dbmap/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.alejandromartin.dbmap
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.findNavController
@@ -11,8 +12,6 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
 import com.alejandromartin.dbmap.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -32,41 +31,55 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
         setSupportActionBar(binding.toolbar)
 
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment_content_main) as NavHostFragment
+
         val navController = navHostFragment.navController
 
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
 
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null)
-                .setAnchorView(R.id.fab).show()
-        }
+        binding.fab.hide()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+
         return when (item.itemId) {
-            R.id.action_settings -> true
+            R.id.menu_medicion -> {
+                navController.navigate(R.id.medirFragment)
+                true
+            }
+
+            R.id.menu_mapa -> {
+                navController.navigate(R.id.mapaFragment)
+                true
+            }
+
+            R.id.menu_historico -> {
+                navController.navigate(R.id.historicoFragment)
+                true
+            }
+
+            R.id.menu_ajustes -> {
+                navController.navigate(R.id.ajustesFragment)
+                true
+            }
+
             else -> super.onOptionsItemSelected(item)
         }
     }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
